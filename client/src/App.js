d72c735edc23e8aa96660393621ed6526a9fc00b
Zamar Zahid <zamerzahid@gmail.com>
2020-08-04 17:51:22 +0500

    All functionalities added.

--- client/src/App.js
+++ client/src/App.js
@@ -19,6 +19,8 @@ import AddExperience from './components/profile-forms/AddExperience';
 import AddEducation from './components/profile-forms/AddEducation';
 import Profiles from './components/profiles/Profiles';
 import Profile from './components/profile/Profile';
+import Posts from './components/posts/Posts';
+import Post from './components/post/Post';
 
 if (localStorage.token) {
   setAuthToken(localStorage.token);
@@ -47,6 +49,8 @@ const App = () => {
               <PrivateRoute exact path='/edit-profile' component={ProfileForm} />
               <PrivateRoute exact path='/add-experience' component={AddExperience} />
               <PrivateRoute exact path='/add-education' component={AddEducation} />
+              <PrivateRoute exact path='/posts' component={Posts} />
+              <PrivateRoute exact path='/posts/:id' component={Post} />
             </Switch>
           </section>
         </Fragment>
